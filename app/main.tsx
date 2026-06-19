50c2ff53de6604bb96eec441614dcb2c827375af
Josafat Guerrero Flores <josafat@hp.starlord.com>
2026-06-19 00:24:36 -0600

    Initial commit

---
+++ app/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, Box, Text } from 'ink';
+
+
+function App(){
+  return (
+    <Box>
+      <Text>Hola mundo</Text>
+    </Box>
+  )
+}
+
+render(<App />)
